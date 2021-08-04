819f6f075ef96d0aff426e61d3f078d7d7eb957f
70G37H3R <56068441+70G37H3R@users.noreply.github.com>
2021-08-04 09:32:22 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -108,7 +108,7 @@ echo
 echo "# ---------------------------------------------------------------------------"
 echo "# Instantiating chaincode: InitLedger"
 echo "# ---------------------------------------------------------------------------"
-docker exec "$CLI_NAME" peer chaincode instantiate -o "$ORDERER_NAME":7050 -C "$CHANNEL_NAME" -n "$CHAINCODE_NAME" "$CHAINCODE_SRC" -v $CHAINCODE_VERSION  -c '{"Args":["InitLedger"]}' -P "AND ('Org1MSP.member', 'Org2MSP.member')" --tls --cafile $ORDERER_CA_LOCATION
+docker exec "$CLI_NAME" peer chaincode instantiate -o "$ORDERER_NAME":7050 -C "$CHANNEL_NAME" -n "$CHAINCODE_NAME" "$CHAINCODE_SRC" -v $CHAINCODE_VERSION  -c '{"Args":["initLedger"]}' -P "AND ('Org1MSP.member', 'Org2MSP.member')" --tls --cafile $ORDERER_CA_LOCATION
 sleep 10 
 
 echo 
@@ -122,11 +122,11 @@ echo
 echo "# ---------------------------------------------------------------------------"
 echo "# Invoking chaincode : Create CAR11"
 echo "# ---------------------------------------------------------------------------"
-docker exec "$CLI_NAME" peer chaincode invoke -o "$ORDERER_NAME":7050 --tls --cafile $ORDERER_CA_LOCATION -C $CHANNEL_NAME -n $CHAINCODE_NAME -c '{"Args":["CreateCar","CAR11","mazda","626","White","Korea"]}'
+docker exec "$CLI_NAME" peer chaincode invoke -o "$ORDERER_NAME":7050 --tls --cafile $ORDERER_CA_LOCATION -C $CHANNEL_NAME -n $CHAINCODE_NAME -c '{"Args":["createCar","CAR11","mazda","626","White","Korea"]}'
 sleep 5
 
 echo 
 echo "# ---------------------------------------------------------------------------"
 echo "# Query chaincode: Query CAR1"
 echo "# ---------------------------------------------------------------------------"
-docker exec "$CLI_NAME" peer chaincode query -o "$ORDERER_NAME":7050 --tls --cafile $ORDERER_CA_LOCATION -C $CHANNEL_NAME -n $CHAINCODE_NAME -c '{"Args":["QueryCar","CAR1"]}'
+docker exec "$CLI_NAME" peer chaincode query -o "$ORDERER_NAME":7050 --tls --cafile $ORDERER_CA_LOCATION -C $CHANNEL_NAME -n $CHAINCODE_NAME -c '{"Args":["queryCar","CAR1"]}'
